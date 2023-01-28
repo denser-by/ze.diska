1788acfac5207f27af17a9ed68d74cba0f13d7de
Dzianis Siarheyeu <denser.by@gmail.com>
2023-01-27 18:58:24 -0800

    panels factory
    expect average

--- mydev/vutils/MegaAverageRec.java
+++ mydev/vutils/MegaAverageRec.java
@@ -1,1 +1,82 @@
-package mydev.vutils; import mydev.vutils.AverageRec; import mydev.vutils.Spiska; public class MegaAverageRec extends AverageRec { protected Spiska ars; public MegaAverageRec() { super(); this.ars=new Spiska();} public void clearItems() { if(ars !=null) for(int i=0; i < ars.size(); i++)((AverageRec) ars.at(i)).clearItems();} public void addItem(AverageRec ar) { ars.append(ar);} public double getAverageTime() { double avgTime=0.f; for(int i=0; i < ars.size(); i++) { double at=((AverageRec) ars.at(i)).getAverageTime(); if(at > 0.f) { avgTime *= i; avgTime+=at; avgTime /=(i+1);}} return avgTime;} public long getRecordsNum() { int num=0; for(int i=0; i < ars.size(); i++) num+=((AverageRec) ars.at(i)).getRecordsNum(); return num;} public double getMinTime() { double minTime=Float.MAX_VALUE; for(int i=0; i < ars.size(); i++) { double mt=((AverageRec) ars.at(i)).getMinTime(); double at=((AverageRec) ars.at(i)).getAverageTime(); if(at > 0.f) { if(mt < minTime) minTime=mt;}} return minTime;} public double getMaxTime() { double maxTime=0.f; for(int i=0; i < ars.size(); i++) { double mt=((AverageRec) ars.at(i)).getMaxTime(); double at=((AverageRec) ars.at(i)).getAverageTime(); if(at > 0.f) { if(mt > maxTime) maxTime=mt;}} return maxTime;}}
+package mydev.vutils;
+
+public final class MegaAverageRec extends AverageRec {
+	private Stack ars;
+	private AverageRec[] arsCopy;
+
+	public MegaAverageRec() {
+		super();
+		this.ars = new Stack();
+		this.arsCopy = makeCopyCash();
+	}
+
+	public void clearItems() {
+		if (ars != null)
+			for (int i = 0; i < arsCopy.length; i++)
+				arsCopy[i].clearItems();
+	}
+
+	public void addItem(AverageRec ar) {
+		if (ar != null) {
+			ars.push(ar);
+			this.arsCopy = makeCopyCash();
+		}
+	}
+
+	public double getAverageTime() {
+		double avgTime = 0.f;
+		int j = 0;
+		for (int i = 0; i < arsCopy.length; i++) {
+			AverageRec ar = arsCopy[i];
+			double at = ar.getAverageTime();
+			if (at > 0.f) {
+				avgTime *= j;
+				avgTime += at;
+				avgTime /= (j + 1);
+				j++;
+			}
+		}
+		return avgTime;
+	}
+
+	public long getRecordsNum() {
+		int num = 0;
+		for (int i = 0; i < arsCopy.length; i++)
+			num += arsCopy[i].getRecordsNum();
+		return num;
+	}
+
+	public double getMinTime() {
+		double minTime = Float.MAX_VALUE;
+		for (int i = 0; i < arsCopy.length; i++) {
+			AverageRec ar = arsCopy[i];
+			double mt = ar.getMinTime();
+			double at = ar.getAverageTime();
+			if (at > 0.f)
+				if (mt < minTime)
+					minTime = mt;
+		}
+		return minTime;
+	}
+
+	public double getMaxTime() {
+		double maxTime = 0.f;
+		for (int i = 0; i < arsCopy.length; i++) {
+			AverageRec ar = arsCopy[i];
+			double mt = ar.getMaxTime();
+			double at = ar.getAverageTime();
+			if (at > 0.f)
+				if (mt > maxTime)
+					maxTime = mt;
+		}
+		return maxTime;
+	}
+
+	private AverageRec[] makeCopyCash() {
+		Object[] queueRecordsArray = ars.getStackRecordsArray();
+		AverageRec[] result = new AverageRec[queueRecordsArray.length];
+		for (int i = 0; i < result.length; i++)
+			result[i] = (AverageRec) queueRecordsArray[i];
+		return result;
+	}
+}
